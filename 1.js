200ed4d103cf1667adda52a27ae4868b74cc34b6
dariashang-code <dariashang@mail.ru>
2025-04-03 12:42:19 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,7 @@
-alert (document.body.firstElementChild);
+alert (documet.body.firstElementChild);
 alert(document.body.children[1]);
 alert(document.body.children[1].lastElementChild);
+log ar = 5
+if ar == 5:
+  ar += 6;
+else ar == 0;
